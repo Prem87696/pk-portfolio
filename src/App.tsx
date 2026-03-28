fec96b4cd1540aaf3d2e61534820fe2465b47ba3
PREM <prem92006@gmail.com>
2026-03-28 23:31:04 +0530

    Add ProtectedRoute for dashboard access control

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 import Auth from './pages/Auth';
 import Dashboard from './pages/Dashboard';
+import ProtectedRoute from './components/ProtectedRoute';
 
 export default function App() {
   return (
@@ -22,12 +23,24 @@ export default function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Layout />}>
+
+              {/* Public Routes */}
               <Route index element={<Home />} />
               <Route path="about" element={<About />} />
               <Route path="projects" element={<Projects />} />
               <Route path="contact" element={<Contact />} />
               <Route path="auth" element={<Auth />} />
-              <Route path="dashboard" element={<Dashboard />} />
+
+              {/* 🔒 Protected Route */}
+              <Route
+                path="dashboard"
+                element={
+                  <ProtectedRoute>
+                    <Dashboard />
+                  </ProtectedRoute>
+                }
+              />
+
             </Route>
           </Routes>
         </BrowserRouter>
